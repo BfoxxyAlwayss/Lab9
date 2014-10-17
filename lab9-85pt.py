f4ff1e04f2ccd27ef22cd861afc961505e21a911
BfoxxyAlwayss <bfoxischaos@gmail.com>
2014-10-17 15:18:08 -0500

    85pt

--- lab9-85pt.py
+++ lab9-85pt.py
@@ -11,6 +11,16 @@ myList = [102,98,96,101,100,99,103,97,98,105]
 
 # Insert for loop here
 
-
+list2 = []
+for x in myList:
+    if x > 100:
+        list2.append(x)
+        
 # This should print [102,101,103,105]
-print myList
+
+print list2 
+
+
+
+
+
